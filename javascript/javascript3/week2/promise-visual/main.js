af3086673f7d1d5c229e102fbd82fb9fc2191162
Ghofrane Ben Hmaid <54797163+benhmaid@users.noreply.github.com>
2020-01-30 19:18:51 +0100

    added javascript folder

--- javascript/javascript3/week2/promise-visual/main.js
+++ javascript/javascript3/week2/promise-visual/main.js
@@ -1,16 +1,20 @@
+const redBox = document.querySelector('ul.marks li:nth-child(1)');
+const blueBox = document.querySelector('ul.marks li:nth-child(2)');
+const greenBox = document.querySelector('ul.marks li:nth-child(3)');
+
 document.querySelector('.btn1').addEventListener('click', translateOneByOne);
 document.querySelector('.btn2').addEventListener('click', translateAllAtOnce);
 
 function translateOneByOne() {
-   moveElement(document.querySelector('li:nth-child(1)'), {
+   moveElement(redBox, {
       x: 20,
       y: 300
    }).then(() => {
-      moveElement(document.querySelector('li:nth-child(2)'), {
+      moveElement(blueBox, {
          x: 400,
          y: 300
       }).then(() =>
-         moveElement(document.querySelector('li:nth-child(3)'), {
+         moveElement(greenBox, {
             x: 400,
             y: 20
          })
@@ -19,20 +23,19 @@ function translateOneByOne() {
 }
 
 function translateAllAtOnce() {
-
    Promise.all([
-    moveElement(document.querySelector('li:nth-child(1)'), {
-        x: 20,
-        y: 300
-    }),
-    moveElement(document.querySelector('li:nth-child(2)'), {
-        x: 400,
-        y: 300
-    }),
-    moveElement(document.querySelector('li:nth-child(3)'), {
-        x: 400,
-        y: 20
-     })
+      moveElement(document.querySelector('li:nth-child(1)'), {
+         x: 20,
+         y: 300
+      }),
+      moveElement(document.querySelector('li:nth-child(2)'), {
+         x: 400,
+         y: 300
+      }),
+      moveElement(document.querySelector('li:nth-child(3)'), {
+         x: 400,
+         y: 20
+      })
    ]).then(() => {
       console.log('All elements has been moved');
    });
