34de7468359f8ceabd6e55e62cd9c5d3b89b4e78
Ghofrane Ben Hmaid <54797163+benhmaid@users.noreply.github.com>
2020-02-12 19:03:35 +0100

    added ..

--- databases/week2/Part3_week2.sql
+++ databases/week2/Part3_week2.sql
@@ -16,6 +16,11 @@ join status on task.status_id = status.id
 where user.name like '%Donald Duck%' and status.name = 'Not started';
 
 -- 3. Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
+select task.id, task.title, task.created, user.name
+from task  
+join user_task on task.id = user_task.task_id
+join user on user.id = user_task.user_id
+where user.name like '%Maryrose Meadows%' and month(created) = 09;
 
 -- 4. Find how many tasks where created in each month, e.g. how many tasks were created in october, how many tasks were created in november, etc. (hint: use group by)
 select month(created) as month, count(task.id) 
