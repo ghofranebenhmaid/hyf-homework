9a06d8358ffa75efd9f9690d81b3a2ea9be938bb
Ghofrane Ben Hmaid <54797163+benhmaid@users.noreply.github.com>
2020-03-27 18:34:32 +0100

    Added components

--- react/week1/todolist/src/index.js
+++ react/week1/todolist/src/index.js
@@ -1,38 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoList from './components/TodoList';
+
+
 import './index.css';
 
-const todos = [
-   {
-      id: 1,
-      description: 'Get out of bed',
-      date: 'Wed Sep 13 2017'
-   },
-   {
-      id: 2,
-      description: 'Brush teeth',
-      date: 'Thu Sep 14 2017'
-   },
-   {
-      id: 3,
-      description: 'Eat breakfast',
-      date: 'Fri Sep 15 2017'
-   }
-];
-class TodoList extends React.Component {
-   render() {
-      const list = todos.map((items) => {
-         return (
-            <div key={items.id} className='todoItems'>
-               <div>{items.description}</div>
-               {items.date}
-            </div>
-         );
-      });
-      return <div className='container'>{list}</div>;
-   }
-}
-class App extends React.Component {
+class TodoApp extends React.Component {
    render() {
       return (
          <div>
@@ -41,5 +14,4 @@ class App extends React.Component {
       );
    }
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
