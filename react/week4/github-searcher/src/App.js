048dd8bfa9670e170d5e5a751989f7e718a23cf2
Ghofrane Ben Hmaid <54797163+benhmaid@users.noreply.github.com>
2020-05-01 16:19:50 +0200

    added files github-searcher

---
+++ react/week4/github-searcher/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Provider } from './context';
+
+import './App.css';
+
+ import Search from './components/Search';
+import ListItem from './components/ListItem';
+
+export default class App extends Component {
+   render() {
+      return (
+         <Provider>
+            <header className='App-header'>
+                <Search /> 
+               <ListItem />
+            </header>
+         </Provider>
+      );
+   }
+}
