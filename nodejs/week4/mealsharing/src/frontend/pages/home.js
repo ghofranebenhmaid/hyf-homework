a10cb880c793c037fac275615babf2a61ae50092
Ghofrane Ben Hmaid <54797163+ghofranebenhmaid@users.noreply.github.com>
2020-08-25 12:30:56 +0200

    Update home.js

--- nodejs/week4/mealsharing/src/frontend/pages/home.js
+++ nodejs/week4/mealsharing/src/frontend/pages/home.js
@@ -18,7 +18,7 @@ window.handleHomeRequest = () => {
         </div>
     </div>
 </header>
-<div style="background: #002065" class="jumbotron bg-cover text-white ">
+<div style="background: #002065" class="jumbotron big-banner bg-cover text-white ">
     <div class="container py-5 text-center ">
         <h1 class="display-4 font-weight-bold">Looking for a Meal</h1>
 
